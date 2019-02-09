920fafeac3d53761d7c831000a63b10dd1ce9992
Jun-Ho Kim <jnhkm311@gmail.com>
2019-02-09 14:35:38 -0800

    Refactor

--- assets/js/retrofy.js
+++ assets/js/retrofy.js
@@ -9,8 +9,4 @@ let divs = document.querySelectorAll("div");
 let index = Math.floor((Math.random() * FILES.length));
 let file = FILES[index + 1];
 
-// for (let i = 0; i < divs.length; i++) {    
-//     divs[i].style.backgroundImage = "url('"+ file + "')"; 
-// }
-
 document.body.style.backgroundImage = "url('"+ file + "')"; 
