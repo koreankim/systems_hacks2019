f2512b1e158b17f7313c1a8f4d237461f682ed63
Jun-Ho Kim <jnhkm311@gmail.com>
2019-02-09 14:11:54 -0800

    Get random images to be displayed

--- assets/js/retrofy.js
+++ assets/js/retrofy.js
@@ -1,4 +1,12 @@
-/* background-image: url("../images/space.png"); */
+const FILES = [
+    'https://i.imgur.com/Z9T53LC.png',
+    'https://i.imgur.com/QDAWkLj.png'
+];
 
-let numFiles = 0;
-let random = Math.floor(Math.random() % numFiles + 1)
+let divs = document.querySelectorAll("div");
+
+for (let i = 0; i < divs.length; i++) {
+    let index = (Math.random() % FILES.length);
+    let file = FILES[index];
+    divs[i].style.backgroundImage = "url('"+ file + "')"; 
+}
